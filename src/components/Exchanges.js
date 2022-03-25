20e7f7046598b87ed3f94d6d6c4d4af19627613f
Garrethhynes <garreth.hynes36@gmail.com>
2022-03-25 18:20:32 +0000

    Crypto App

---
+++ src/components/Exchanges.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exchanges = () => {
+    return (
+        <div>
+            This API has now expired - data no longer available
+        </div>
+    )
+}
+
+export default Exchanges
